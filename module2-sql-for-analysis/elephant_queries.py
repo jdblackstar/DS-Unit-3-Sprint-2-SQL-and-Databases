4ab9a825ce86798e0494dd95087f56a5c37b9014
Josh Black-Star <josh@blackstar.dev>
2020-05-12 13:36:53 -0700

    add elephant_multi

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -5,14 +5,15 @@ DB_USER="ixcugnqx"
 DB_PW="7G_ii_FI2UJZIsFuhIzy7eA0eUB6Eylp"
 DB_HOST="hansken.db.elephantsql.com"
 
-conn = psycho.connect(dbname=DB_NAME, user=DB_USER, password=DB_PW, host=DB_HOST)
-print(type(connection))
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PW, host=DB_HOST)
+print(type(connection)) #> <class 'psycopg2.extensions.connection'>
 
 cursor = connection.cursor()
-print(type(cursor))
+print(type(cursor)) #> <class 'psycopg2.extensions.cursor'>
 
-breakpoint()
+cursor.execute("SELECT * from test_table;") #TODO: share links related to this two step process
 
-cursor.execute("SELECT * from test_table;")
-
-results = cursor.fetchone()
+#results = cursor.fetchone()
+results = cursor.fetchall()
+for row in results:
+    print(type(row), row)
